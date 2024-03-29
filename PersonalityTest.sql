dddc75cd02f770b8e05a07ffd0e28112ddaa99c3
Herman Liao (hl01234) <hl01234@student.ubc.ca>
2024-03-29 15:12:19 -0700

    Updated SQL code to be a completed version of the incomplete milestone 2 code

--- PersonalityTest.sql
+++ PersonalityTest.sql
@@ -1,12 +1,11 @@
 CREATE TABLE User (
-    username INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(63) NOT NULL,
     PRIMARY KEY (username),
 );
 
 CREATE TABLE LoginUser (
-    username INT,
-    mbtiName CHAR(4),
-    username VARCHAR(255) NOT NULL,
+    mbtiName CHAR(4) NOT NULL,
+    username VARCHAR(63) NOT NULL,
     password VARCHAR(255) NOT NULL,
     emailAddress VARCHAR(255) NOT NULL,
     age INT,
@@ -18,105 +17,91 @@ CREATE TABLE LoginUser (
     FOREIGN KEY (mbtiName) REFERENCES MBTI_Type(mbtiName) ON DELETE CASCADE
 );
 
-CREATE TABLE Test (
-    TID INT NOT NULL AUTO_INCREMENT,
-    RID INT NOT NULL,
-    username INT NOT NULL,
-    startTime DATETIME NOT NULL,
-    testGender CHAR(1),
-    PRIMARY KEY (TID),
-    UNIQUE (startTime, username),
-    FOREIGN KEY (username) REFERENCES User(username) ON DELETE CASCADE
-    FOREIGN KEY (RID) REFERENCES Report_2(RID) ON DELETE CASCADE
-);
-
 CREATE TABLE Question (
     questionNumber INT NOT NULL AUTO_INCREMENT,
     TID INT NOT NULL,
     questionDescription TEXT NOT NULL,
-    questionAnswer INT NOT NULL,PRIMARY KEY (questionNumber, TID),
+    questionAnswer INT NOT NULL,
+    PRIMARY KEY (questionNumber, TID),
     FOREIGN KEY (TID) REFERENCES Test(TID) ON DELETE CASCADE
 );
 
-CREATE TABLE Outputs_3 (
+CREATE TABLE Outputs_2 (
     TID INT NOT NULL,
-    EIScore REAL,
-    SNScore REAL,
-    TFScore REAL,
-    JPScore REAL,
-    mbtiName INT NOT NULL,
-    PRIMARY KEY (EIScore, EIScore, EIScore, EIScore),
-    FOREIGN KEY (EIScore, EIScore, EIScore, EIScore) REFERENCES
-    Outputs_4(EIScore, EIScore, EIScore, EIScore) ON DELETE CASCADE
-);
+    RID INT NOT NULL,
+    startDateTime DATETIME NOT NULL,
+    username VARCHAR(63) NOT NULL,
+    PRIMARY KEY (TID)
+)
 
 CREATE TABLE Outputs_3 (
     TID INT NOT NULL,
-    EIScore REAL,
-    SNScore REAL,
-    TFScore REAL,
-    JPScore REAL,
-    mbtiName INT NOT NULL,
-    PRIMARY KEY (EIScore, EIScore, EIScore, EIScore),
-    FOREIGN KEY (EIScore, EIScore, EIScore, EIScore) REFERENCES
-    Outputs_4(EIScore, EIScore, EIScore, EIScore) ON DELETE CASCADE
-);
-
-CREATE TABLE Report_2 (
-    RID INT NOT NULL AUTO_INCREMENT,
-    TID INT NOT NULL,
-    username INT NOT NULL,
-    mbtiName INT NOT NULL,
     EIScore REAL NOT NULL,
     SNScore REAL NOT NULL,
     TFScore REAL NOT NULL,
     JPScore REAL NOT NULL,
-    reportGender CHAR(1) NOT NULL,
-    PRIMARY KEY (RID),
-    FOREIGN KEY (TID) REFERENCES Test(TID) ON DELETE CASCADE,
-    FOREIGN KEY (EIScore) REFERENCES Report_1(EIScore) ON DELETE CASCADE,
-    FOREIGN KEY (SNScore) REFERENCES Report_1(SNScore) ON DELETE CASCADE,
-    articleTitle TEXT,
-    articleAuthor TEXT,
-    PRIMARY KEY (AID),
-    UNIQUE (articleURL),
-    FOREIGN KEY (articleTitle) REFERENCES
-    Article_1(articleTitle) ON DELETE CASCADE,
-    FOREIGN KEY (articleAuthor) REFERENCES
-    Article_2(articleAuthor) ON DELETE CASCADE
+    PRIMARY KEY (TID, EIScore, SNScore, TFScore, JPScore),
+    FOREIGN KEY (EIScore, SNScore, TFScore, JPScore) REFERENCES Outputs_4(EIScore, SNScore, TFScore, JPScore) ON DELETE CASCADE,
+    FOREIGN KEY (TID) REFERENCES Outputs_2(TID) ON DELETE CASCADE
 );
 
+CREATE TABLE Outputs_4 (
+    mbtiName CHAR(4) NOT NULL,
+    EIScore REAL NOT NULL,
+    SNScore REAL NOT NULL,
+    TFScore REAL NOT NULL,
+    JPScore REAL NOT NULL,
+    PRIMARY KEY (EIScore, SNScore, TFScore, JPScore)
+);
+
+CREATE TABLE MBTI_Type (
+    mbtiName CHAR(4) NOT NULL,
+    mbtiDescription TEXT NOT NULL,
+    PRIMARY KEY (mbtiName)
+)
+
 CREATE TABLE Video (
-    VID INT NOT NULL AUTO_INCREMENT,
-    videoLink VARCHAR(255),
-    videoType VARCHAR(255) NOT NULL,
+    videoLink VARCHAR(255) NOT NULL,
+    videoType VARCHAR(255),
     videoTitle VARCHAR(255) NOT NULL,
-    PRIMARY KEY (VID)
+    PRIMARY KEY (videoLink)
+);
+
+CREATE TABLE Book (
+    bookURL VARCHAR(255) NOT NULL,
+    bookTitle VARCHAR(255) NOT NULL,
+    bookAuthor VARCHAR(255) NOT NULL,
+    PRIMARY KEY (bookURL)
+);
+
+CREATE TABLE Article (
+    articleURL VARCHAR(255) NOT NULL,
+    articleTitle VARCHAR(255) NOT NULL,
+    articleAuthor VARCHAR(255) NOT NULL,
+    articleText TEXT NOT NULL,
+    PRIMARY KEY (articleURL)
 );
 
 CREATE TABLE IsRecommendedVideo (
-    mbtiName INT,
-    VID INT,
-    PRIMARY KEY (mbtiName, VID),
-    FOREIGN KEY (mbtiName) REFERENCES MBTI_Type_2(mbtiName) ON
-    DELETE CASCADE,
-    FOREIGN KEY (VID) REFERENCES Video(VID) ON DELETE CASCADE
+    mbtiName CHAR(4) NOT NULL,
+    videoLink VARCHAR(255) NOT NULL,
+    PRIMARY KEY (mbtiName, videoLink),
+    FOREIGN KEY (mbtiName) REFERENCES MBTI_Type(mbtiName) ON DELETE CASCADE,
+    FOREIGN KEY (videoLink) REFERENCES Video(videoLink) ON DELETE CASCADE
 );
 
 CREATE TABLE IsRecommendedBook (
-    mbtiName INT,
-    BID INT,
-    PRIMARY KEY (mbtiName, BID),
-    FOREIGN KEY (mbtiName) REFERENCES MBTI_Type_2(mbtiName) ON
-    DELETE CASCADE,
-    FOREIGN KEY (BID) REFERENCES Book(BID) ON DELETE CASCADE
+    mbtiName CHAR(4) NOT NULL,
+    bookURL VARCHAR(255) NOT NULL,
+    PRIMARY KEY (mbtiName, bookURL),
+    FOREIGN KEY (mbtiName) REFERENCES MBTI_Type(mbtiName) ON DELETE CASCADE,
+    FOREIGN KEY (bookURL) REFERENCES Book(bookURL) ON DELETE CASCADE
 );
 
 CREATE TABLE IsRecommendedArticle (
-    mbtiName INT,
-    AID INT,
-    PRIMARY KEY (mbtiName, AID),
-    FOREIGN KEY (mbtiName) REFERENCES MBTI_Type_2(mbtiName) ON
-    DELETE CASCADE,
-    FOREIGN KEY (AID) REFERENCES Article(AID) ON DELETE CASCADE
+    mbtiName CHAR(4) NOT NULL,
+    articleURL VARCHAR(255) NOT NULL,
+    PRIMARY KEY (mbtiName, articleURL),
+    FOREIGN KEY (mbtiName) REFERENCES MBTI_Type(mbtiName) ON DELETE CASCADE,
+    FOREIGN KEY (articleURL) REFERENCES Article(articleURL) ON DELETE CASCADE
 );
